4d4cd0b71e344d1823a5de2fcdc996c0867201e5
Fuwen Tan <fuwen.tan@gmail.com>
2024-09-02 16:33:19 +0100

    test/cpu -> cu121

--- install_requirements.sh
+++ install_requirements.sh
@@ -62,7 +62,7 @@ done
 NIGHTLY_VERSION=dev20240507
 
 # The pip repository that hosts nightly torch packages.
-TORCH_URL="https://download.pytorch.org/whl/test/cpu"
+TORCH_URL="https://download.pytorch.org/whl/cu121"
 
 # pip packages needed by exir.
 EXIR_REQUIREMENTS=(
